e72aac46583ed058f7fc23d3224208aba4854dc0
8chan <hachiro_doi316@yahoo.co.jp>
2019-04-04 05:05:53 +0900

    Support P-prefix address

--- BRAddress.h
+++ BRAddress.h
@@ -40,7 +40,7 @@ extern "C" {
 
 // bitcoin address prefixes
 #define BITCOIN_PUBKEY_ADDRESS      50
-#define BITCOIN_SCRIPT_ADDRESS      5
+#define BITCOIN_SCRIPT_ADDRESS      55
 #define BITCOIN_PUBKEY_ADDRESS_TEST 111
 #define BITCOIN_SCRIPT_ADDRESS_TEST 196
 
